17bfcf53633e722e138a4fd69831ee6caafef4d8
diksha-signity <34769773+diksha-signity@users.noreply.github.com>
2020-07-15 19:59:50 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,6 @@ JarName="webpack-dev-server"
 if [[ "$output" == "$JarName" ]]; then
  kill -9 $(ps aux | grep node | grep 'webpack-dev-server' | awk '{print$2}')
 fi
-cd /home/dev/jitsi-meet/
+cd /home/dev/test-jitsi-vidi
 nohup make dev >/dev/null 2>&1 &
 exit 0
