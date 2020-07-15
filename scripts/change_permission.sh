5f87a82f807c23a15918d6cb7427fab84c5f598f
diksha-signity <34769773+diksha-signity@users.noreply.github.com>
2020-07-15 19:59:24 +0530

    Update change_permission.sh

--- scripts/change_permission.sh
+++ scripts/change_permission.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/dev/
-chown -R dev.dev /home/dev/jitsi-meet
+chown -R dev.dev /home/dev/test-jitsi-vidi
 cd jitsi-meet
 find . -type d -exec chmod 0755 {} \;
 find . -type f -exec chmod 0644 {} \;
